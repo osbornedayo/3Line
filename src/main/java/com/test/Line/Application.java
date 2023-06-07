9efc25bd8540bf7d746b7085c32dc0b2eec82e74
Osborne Dayo <oluwadyo.osborne@seerbit.com>
2023-06-07 15:02:05 +0100

    more updates

--- src/main/java/com/test/Line/Application.java
+++ src/main/java/com/test/Line/Application.java
@@ -11,29 +11,20 @@ public class Application {
 
 		BitWise bitWise=new BitWise();
 
-		int[] arr = new int[] {2,4,2,9,1,3,0};
-		int[] arr2 = new int[] {3,4,2,4,1,3,6};
+		int[] array1 = new int[] {2,4,2,9,1,3,0};
+		int[] array2 = new int[] {3,4,2,4,1,3,6};
 
-		bitWise.encodeArray(arr, arr2);
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(arr[i] + " ");
+		bitWise.encodeArray(array1, array2);
+		for(int i=0; i<array1.length; i++) {
+			System.out.print("This is the Encoded Array "+array1[i] + " ");
 		}
 
 		System.out.println();
 
-		bitWise.decodeArray(arr, arr.length);
-		for(int i=0; i<arr.length; i++) {
-			System.out.print(arr[i] + " ");
+		bitWise.decodeArray(array1, array1.length);
+		for(int i=0; i<array1.length; i++) {
+			System.out.print("This is decoded array  "+array1[i] + " ");
 		}
-
-
-//		Regarding the classification of the algorithm in terms of Big O notation,
-//		the encodeArray function has a time complexity of O(n) because it iterates
-//		over each element of the arrays A and B. The decodeArray function also has
-//		a time complexity of O(n) because it performs the reverse process for each element.
-//		Therefore, both the encoding and decoding processes
-//		have a linear time complexity, O(n), where n is the size of the arrays
-//
 
 	}
 
